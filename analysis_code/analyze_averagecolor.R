7c1b84e4037ed234fb9bba68296bd46bac1dec19
Marisa Agarwal <marisaagarwal@gmail.com>
2022-02-09 14:26:23 +1000

    creating average color analysis

---
+++ analysis_code/analyze_averagecolor.R
@@ -0,0 +1,24 @@
+# 2022-02-09
+
+
+## 1. Set up
+
+  # point to data locale
+  data_locale = "creation_code/"
+  
+  # load in the data
+  source(paste0(data_locale, "create_CoralWatch_data.R"))
+  
+  # check data structure
+  str(CoralWatch_data)
+
+  
+## 2. Summarize data for average color score by each survey
+  
+  CoralWatch_average_colorcodes =   
+    CoralWatch_data %>%
+      group_by(site, date_time, water_temp_C, coral_type) %>%
+      summarise(average_colorcode = mean(colorcode_average))
+
+  
+  
